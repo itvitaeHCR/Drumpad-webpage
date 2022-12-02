d1bb42877adbd3208715fc9a3462244fe1fa327e
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-12-02 17:45:32 +0100

    drumpad v1

---
+++ scripts.js
@@ -0,0 +1,54 @@
+// CRASH
+document.getElementById("crash").addEventListener("click", function(){ 
+    let crash = document.getElementById("acoustic-crash");
+    crash.play(); 
+}); 
+
+// HIHAT
+document.getElementById("hihat").addEventListener("click", function(){ 
+    let hihat = document.getElementById("acoustic-hihat");
+    hihat.play(); 
+}); 
+
+// SPLASH
+document.getElementById("splash").addEventListener("click", function(){ 
+    let splash = document.getElementById("acoustic-splash");
+    splash.play(); 
+}); 
+
+// RIDE
+document.getElementById("ride").addEventListener("click", function(){ 
+    let ride = document.getElementById("acoustic-ride");
+    ride.play(); 
+}); 
+
+// SNARE
+document.getElementById("snare").addEventListener("click", function(){ 
+    let snare = document.getElementById("acoustic-snare");
+    snare.play(); 
+}); 
+
+// HIGH TOM
+document.getElementById("high-tom").addEventListener("click", function(){ 
+    let high_tom = document.getElementById("acoustic-high-tom");
+    high_tom.play(); 
+}); 
+
+// LOW TOM
+document.getElementById("low-tom").addEventListener("click", function(){ 
+    let low_tom = document.getElementById("acoustic-low-tom");
+    low_tom.play(); 
+}); 
+
+// FLOOR TOM
+document.getElementById("floor-tom").addEventListener("click", function(){ 
+    let floor_tom = document.getElementById("acoustic-floor-tom");
+    floor_tom.play(); 
+}); 
+
+// BASS
+document.getElementById("bass").addEventListener("click", function(){ 
+    let bass = document.getElementById("acoustic-bass");
+    bass.play(); 
+}); 
+
